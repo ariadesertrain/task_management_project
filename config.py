22636e01e1d29cd66881ecb9788478f8864cbb01
ariadesertrain <mariburdelis@gmail.com>
2026-01-25 12:05:07 -0800

    Added "requirements.txt" and "config.py"

---
+++ config.py
@@ -0,0 +1,4 @@
+#This file tells the application where the database is located so the application knows where to ask for the data.
+
+#Database connection string (this is where the database is located)
+database_url = "postgresql://user:password@localhost/db"
