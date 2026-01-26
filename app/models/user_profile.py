3eb552720cf33fc960a139f4d8e6b1146bc7abd2
ariadesertrain <mariburdelis@gmail.com>
2026-01-25 17:46:15 -0800

    Started working on the database and updated requirements.txt

---
+++ app/models/user_profile.py
@@ -0,0 +1,8 @@
+#Import data types and Column class from SQLAlchemy
+from sqlalchemy import Column,Integer,String,Boolean,DateTime
+
+#Import SQL functions
+from sqlalchemy.sql import func
+
+#Imports the Base class from database.py
+
